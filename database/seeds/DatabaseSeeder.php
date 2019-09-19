6bae1be4c59ddf4fd2d0f20e3aa45a6a3375b542
Dipayan Sen Gupta <dip.himu@gmail.com>
2019-09-19 17:14:33 +0600

    Change seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Model;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -9,8 +9,16 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    protected $toTruncate = ['products','admins','users','sales','writers','histories'];
     public function run()
     {
+        Model::unguard();
+        foreach($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+        }
         $this->call(UsersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+
+        Model::reguard();    
     }
 }
---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($x = 0; $x <= 50; $x++) {
+            factory(App\Product::class)->create();
+        }
+    }
+}
